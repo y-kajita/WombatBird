bd8d7ea1dfe4a530f971d700a230d36254a3c898
murapong <murapong@gmail.com>
2016-12-04 16:49:15 +0900

    初期化漏れ

--- Assets/Scripts/Main/GameManager.cs
+++ Assets/Scripts/Main/GameManager.cs
@@ -4,28 +4,33 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
-
-	public Text gameover_text;
-	public GameObject gameover_button;
-	public static bool gameover;
-	float endtime;
-	// Use this for initialization
-	void Start () {
-		
-	}
+public class GameManager : MonoBehaviour
+{
+    public Text gameover_text;
+    public GameObject gameover_button;
+    public static bool gameover;
+    float endtime;
+    // Use this for initialization
+    void Start()
+    {
+        ScoreManagerScript.score = 0;
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		if (gameover) {
-			Time.timeScale = 0.0f;
-		}
-		gameover_text.enabled = gameover;
-		gameover_button.SetActive(gameover);
-	}
-	public void BackToTitle(){
-		Debug.Log ("Back");
-		SceneManager.LoadScene(SceneName.Title);
-		gameover = false;
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameover)
+        {
+            Time.timeScale = 0.0f;
+        }
+        gameover_text.enabled = gameover;
+        gameover_button.SetActive(gameover);
+    }
+
+    public void BackToTitle()
+    {
+        Debug.Log("Back");
+        SceneManager.LoadScene(SceneName.Title);
+        gameover = false;
+    }
 }
