af25428f697f0b7ef81f1f2007ce21b94dc6bc9f
murapong <murapong@gmail.com>
2016-12-04 16:08:04 +0900

    ランキング画面の調整

--- Assets/Scripts/Ranking/RankingSceneController.cs
+++ Assets/Scripts/Ranking/RankingSceneController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.UI;
 
 public class RankingSceneController : MonoBehaviour
 {
@@ -17,6 +18,36 @@ public class RankingSceneController : MonoBehaviour
     #endregion
 
     #region private property
+
+    [SerializeField]
+    Text rank1Name;
+
+    [SerializeField]
+    Text rank1Score;
+
+    [SerializeField]
+    Text rank2Name;
+
+    [SerializeField]
+    Text rank2Score;
+
+    [SerializeField]
+    Text rank3Name;
+
+    [SerializeField]
+    Text rank3Score;
+
+    [SerializeField]
+    Text rank4Name;
+
+    [SerializeField]
+    Text rank4Score;
+
+    [SerializeField]
+    Text rank5Name;
+
+    [SerializeField]
+    Text rank5Score;
 
     #endregion
 
@@ -44,13 +75,20 @@ public class RankingSceneController : MonoBehaviour
 
         string[,] RankTbl = Obj.GetRankingInfo();
 
-        //  僩僢僾5埵偺傒昞帵
-        for (int LineNo = 0; LineNo < 5; LineNo++)
-        {
-            Debug.Log(RankTbl[LineNo, 0].ToString() + " - " + RankTbl[LineNo, 1].ToString() + " - " + RankTbl[LineNo, 2].ToString());
-        }
+        rank1Name.text = RankTbl[0, 1].ToString();
+        rank1Score.text = RankTbl[0, 2].ToString();
 
-        System.Console.WriteLine("");
+        rank2Name.text = RankTbl[1, 1].ToString();
+        rank2Score.text = RankTbl[1, 2].ToString();
+
+        rank3Name.text = RankTbl[2, 1].ToString();
+        rank3Score.text = RankTbl[2, 2].ToString();
+
+        rank4Name.text = RankTbl[3, 1].ToString();
+        rank4Score.text = RankTbl[3, 2].ToString();
+
+        rank5Name.text = RankTbl[4, 1].ToString();
+        rank5Score.text = RankTbl[4, 2].ToString();
     }
 
     #endregion
